c41e9dd5f922f685f06e2022c11869a06fcfe192
Walter Wang <walter@localhost.com>
2017-07-10 12:31:35 +0800

    Add User page comment and likes

---
+++ user-page-comments.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+import requests
+import pandas as pd 
+from dateutil.parser import parse
+
+import config
+
+#在Facebook Graph API Exploer取得token
+token = config.token
+fanpage_id = config.fanpage_id
+fanpage_name = config.fanpage_name
+
+what_page_id = config.filter_page_id
+if len(sys.argv) > 1:
+    what_page_id = sys.argv[1]
+
+    #在Facebook Graph API Exploer取得粉絲專頁的id與名稱，並將其包成字典dic
+    fanpage = {fanpage_id : fanpage_name} 
+
+    #建立一個空的list
+    information_list = []
+
+    out_file = 'who-comments-{0}.csv'.format(what_page_id)
+
+    res = requests.get('https://graph.facebook.com/v2.9/{}/comments?summary=true&access_token={}'.format(what_page_id, token))
+
+    i = 0
+
+    #API最多一次呼叫100筆資料，因此使用while迴圈去翻頁取得所有的資料
+    while 'paging' in res.json(): 
+        for information in res.json()['data']:
+            if 'message' in information:
+                what_page = '"http://www.facebook.com/{0}"'.format(what_page_id)
+                fb_user_link = '"http://www.facebook.com/{0}"'.format(information['from']['id'])
+                fb_user_name = '"%s"' % (information['from']['name'])
+                fb_user_message = '"%s"' % (information['message'])
+                created_time = '"{0}"'.format(parse(information['created_time']).date())
+                information_list.append([i, what_page, fb_user_link, fb_user_name, fb_user_message, created_time])
+                i += 1
+        if 'next' in res.json()['paging']:
+            res = requests.get(res.json()['paging']['next'])
+        else:
+            break
+
+    #最後將list轉換成dataframe，並輸出成csv檔
+    information_df = pd.DataFrame(information_list, columns=['序號', '粉絲專頁', '臉書連結', '姓名', '留言內容', '留言時間']) 
+    information_df.to_csv(out_file, index=False, sep=',', encoding='utf-8', quoting=csv.QUOTE_NONE)
+else:
+    print 'Usage: python {0} <page-id>'.format(sys.argv[0])
---
+++ user-page-likes.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+import requests
+import pandas as pd 
+from dateutil.parser import parse
+
+import config
+
+#在Facebook Graph API Exploer取得token
+token = config.token
+fanpage_id = config.fanpage_id
+fanpage_name = config.fanpage_name
+
+what_page_id = config.filter_page_id
+if len(sys.argv) > 1:
+    what_page_id = sys.argv[1]
+
+    #在Facebook Graph API Exploer取得粉絲專頁的id與名稱，並將其包成字典dic
+    fanpage = {fanpage_id : fanpage_name} 
+
+    #建立一個空的list
+    information_list = []
+
+    out_file = 'who-likes-{0}.csv'.format(what_page_id)
+
+    res = requests.get('https://graph.facebook.com/v2.9/{}/likes?summary=true&access_token={}'.format(what_page_id, token))
+
+    #API最多一次呼叫100筆資料，因此使用while迴圈去翻頁取得所有的資料
+    i = 0
+    while 'paging' in res.json(): 
+        for information in res.json()['data']:
+            if 'id' in information:
+                what_page = '"http://www.facebook.com/{0}"'.format(what_page_id)
+                fb_user_link = '"http://www.facebook.com/{0}"'.format(information['id'])
+                fb_user_name = '"%s"' % (information['name'])
+                information_list.append([i, what_page, fb_user_link, fb_user_name])
+                i += 1
+
+        if 'next' in res.json()['paging']:
+            res = requests.get(res.json()['paging']['next'])
+        else:
+            break
+
+    #最後將list轉換成dataframe，並輸出成csv檔
+    information_df = pd.DataFrame(information_list, columns=['序號', '粉絲專頁', '臉書連結', '姓名']) 
+    information_df.to_csv(out_file, index=False, sep=',', encoding='utf-8', quoting=csv.QUOTE_NONE)
+else:
+    print 'Usage: python {0} <page-id>'.format(sys.argv[0])
